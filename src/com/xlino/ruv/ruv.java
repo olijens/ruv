7143337014f43934f66166b4c65229bc6e1776ee
Ólafur Jens Sigurðsson <ojsbug@gmail.com>
2013-08-14 01:54:41 +0000

    fyrsta útgáfa, nota webview og mobile útgáfu rúv vefsins

--- src/com/xlino/ruv/ruv.java
+++ src/com/xlino/ruv/ruv.java
@@ -2,14 +2,23 @@ package com.xlino.ruv;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
 
 public class ruv extends Activity
 {
+    private WebView webview;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        webview = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("http://m.ruv.is");
     }
 }
